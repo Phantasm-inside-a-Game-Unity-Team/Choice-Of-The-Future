10aed805da6aacfb6a0ea9dea6e541a6e83b7072
wuxucheng1111 <wuxucheng1111@gmail.com>
2019-11-19 01:21:56 +0900

    feat:增加了场景切换功能，从InitialScene开始，从地图正上方进入第二个场景

---
+++ Assets/Tests/CJPH/Scripts/MySceneManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySceneManager : SingletonTemplate<MySceneManager>
+{
+    public GameObject playerPrefeb;
+    public GameObject player;
+    public Vector3 playerInitialPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SceneStart()
+    {
+        if (player == null)
+        {
+            if (playerPrefeb == null)
+            {
+                playerPrefeb = Resources.Load("Reimu") as GameObject;
+
+            }
+            player = Instantiate<GameObject>(playerPrefeb, playerInitialPosition, Quaternion.identity);
+        }
+        else
+        {
+            player.transform.position = playerInitialPosition;
+        }
+    }
+}
---
+++ Assets/Tests/CJPH/Scripts/SingletonTemplate.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class SingletonTemplate<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static volatile T instance;
+    private static object syncRoot = new Object();
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        T[] instances = FindObjectsOfType<T>();
+                        if (instances.Length != 0)
+                        {
+                            instance = instances[0];
+                            for (var i = 1; i < instances.Length; i++)
+                            {
+                                Destroy(instances[i].gameObject);
+                            }
+                        }
+                        else
+                        {
+                            GameObject go = new GameObject();
+                            go.name = typeof(T).Name;
+                            instance = go.AddComponent<T>();
+                            DontDestroyOnLoad(go);
+                        }
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
